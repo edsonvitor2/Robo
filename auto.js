36fc45b81cb21be3f949ec87ce3d354b05035931
Edson Vitor de Sousa <82064658+edsonvitor2@users.noreply.github.com>
2024-02-29 14:49:47 -0300

    acionando
    robo ja faz acionamento e confirma

--- auto.js
+++ auto.js
@@ -29,7 +29,7 @@ class Robo {
             
             await this.delay(5000); // Espera 5 segundos
 
-            await page.type('[id="txt_CollectSearch"]', '36015470'); // 36015470 erro de 2 contratos ou mais 
+            await page.type('[id="txt_CollectSearch"]', '13185479'); // 36015470 erro de 2 contratos ou mais 
             console.log('Pesquisar após 5 segundos');
 
             await page.keyboard.press('Enter');
@@ -42,11 +42,11 @@ class Robo {
             // Verifica se algum dos links tem o texto "nome"
             for (const link of links) {
                 const textoLink = await page.evaluate(element => element.textContent.trim(), link);
-                if (textoLink === 'A A PEREIRA SIQUEIRA') {
+                if (textoLink === 'A C LINO TRANSPORTE ME') {
                     // Tenta clicar no link com o texto "nome"
                     try {
                         await link.click();
-                        console.log('Clicou no link com o texto "A A PEREIRA SIQUEIRA".');
+                        console.log('Clicou no link com o texto "A C LINO TRANSPORTE ME".');
                         await this.delay(5000); // Espera 5 segundos
                         break;
                     } catch (error) {
@@ -55,28 +55,30 @@ class Robo {
                     }
                 }
             }
-            console.log('continuando!!')
-            
-                /*
-                let collectOmniIndexPlaceHolder;
-            try {
-                collectOmniIndexPlaceHolder = await page.$('#CollectOmniIndexPlaceHolder');
-                console.log('mais de 1 contrato encontrado')
-            } catch (error) {
-                console.error('Erro ao verificar a existência do elemento:', error);
-            }
-            
+
+            console.log('continuando!!');
+
             const conteudoTagsB = await page.$$eval('b', elements => elements.map(element => element.textContent.trim()));
 
                 const textoEsperado = 'Contas em Assessorias Externas - NÃO DISPONÍVEIS';
                 
                 if (conteudoTagsB.includes(textoEsperado)) {
                     console.log(`O texto "${textoEsperado}" foi encontrado dentro das tags <b>.`);
-                    // Faça algo se o texto for encontrado
+                    await this.delay(5000); // Espera 5 segundos
+                    await page.goto('https://alpheratz.itapevarec.com.br/Alpheratz/login.aspx');
                 } else {
                     console.log(`O texto "${textoEsperado}" não foi encontrado dentro das tags <b>.`);
-                    // Faça algo se o texto não for encontrado
-                }*/
+
+                    this.selecionarMensagem();
+                    let mensagem = this.selecionarMensagem();
+                    console.log(mensagem);
+                    await page.type('[id="TaskResults"]', `${mensagem}`);
+
+                    await this.delay(15000);
+                    await page.click('[id="bt_SaveWorkflowTracking"]');
+
+                }
+            
         } catch (erro) {
             console.error('Ocorreu um erro:', erro);
         }
@@ -85,6 +87,41 @@ class Robo {
     delay(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
+
+    // Função para selecionar aleatoriamente uma mensagem com base nas porcentagens
+    selecionarMensagem() {
+// Array com as mensagens disponíveis
+const mensagensGrupo1 = [
+    'Telefone ocupado',
+    'Telefone não atende',
+    'Telefone indisponível no momento',
+    'Contato desligou',
+    'Desconhece o cliente',
+    'Ligação muda'
+];
+
+const mensagensGrupo2 = [
+    'Ligação interrompida/ruim',
+    'Solicita contato outro dia/hora',
+    'Recado'
+];
+
+// Função para selecionar aleatoriamente uma mensagem com base nas porcentagens
+    const random = Math.random(); // Gerar um número aleatório entre 0 e 1
+
+    // Selecionar uma mensagem com base na porcentagem
+    if (random > 0.2) {
+        // 80% das vezes, seleciona uma mensagem do primeiro grupo
+        return mensagensGrupo1[Math.floor(Math.random() * mensagensGrupo1.length)];
+    } else if(random <= 0.2) {
+        // 20% das vezes, seleciona uma mensagem do segundo grupo
+        return mensagensGrupo2[Math.floor(Math.random() * mensagensGrupo2.length)];
+    }
+
+// Chame a função selecionarMensagem() para obter uma mensagem aleatória
+console.log('Mensagem selecionada:', mensagem);
+
+}
 
 }
 
