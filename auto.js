5c776efb124026c4220d239115f4e4dbe4a9a047
Edson Vitor de Sousa <82064658+edsonvitor2@users.noreply.github.com>
2024-03-12 18:04:02 -0300

    automatização finalizada falta interface
    agora so falta terminar a interface e relatorios de dados

--- auto.js
+++ auto.js
@@ -1,7 +1,10 @@
 class Robo {
     constructor() {
+        // Instancia o Puppeteer e o MSSQL
         this.puppeteer = require('puppeteer'); // instancia puppeteer
         this.sql = require('mssql');
+
+        // Configurações de conexão com o banco de dados
         this.config={
             user: 'sa',
             password: 'etropus@147258',
@@ -11,126 +14,205 @@ class Robo {
                 encrypt: false // Se necessário
             },
         }
+
+        // Variável para armazenar informações do cliente
+        this.cliente;
     }
 
     async iniciarRobo(usuario) {
         try {
+            // Inicia o navegador Puppeteer
             const browser = await this.puppeteer.launch({ headless: false }); // Instancia o navegador 
             const page = await browser.newPage(); // Abre uma nova página
             await page.goto('https://alpheratz.itapevarec.com.br/Alpheratz/login.aspx'); // Abre o site 
-            this.delay(5000)
-            await page.waitForSelector('[id="txt_username"]'); // Espera o site carregar o campo de login
-            await page.type('[name="txt_username"]', `${usuario.usuario}`); // Digita o CPF/CNPJ 
-            await page.type('[name="txt_password"]', `${usuario.senha}`); // Digita a senha 
-            await page.keyboard.press('Enter');
 
+            // Espera o site carregar o campo de login
+            await page.waitForSelector('[id="txt_username"]');
+            // Digita o CPF/CNPJ e a senha
+            await page.type('[name="txt_username"]', `${usuario.usuario}`);
+            await page.type('[name="txt_password"]', `${usuario.senha}`);
+            await page.keyboard.press('Enter'); // Pressiona Enter
+
+            // Variáveis para controlar o número de iterações e o horário atual
             var acionamentos = 80;
             var contador = 0;
-    
+
             while (contador < acionamentos) { // Loop vai rodar enquanto houver clientes
                 const dataAtual = new Date();
                 const hora = dataAtual.getHours();
                 const minutos = dataAtual.getMinutes();
                 const segundos = dataAtual.getSeconds();
-    
-                const horario = `${hora.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`;
-    
+                const horario = '09:00:00';
+
                 let intervalo_inicio = usuario.hora_intervalo_inicio;
                 let intervalor_fim = usuario.hora_intervalo_fim;
                 let inicio = usuario.hora_inicio;
                 let fim = usuario.hora_fim;
-    
+
+                // Verifica se está dentro do horário de trabalho
                 if (horario > inicio && horario < fim) {
+                    // Verifica se está dentro do intervalo de lanche
                     if (horario > intervalo_inicio && horario < intervalor_fim) {
                         console.log('Horario de lanche');
                         break;
                     } else {
                         console.log('Iniciando');
 
-                        await page.waitForSelector('[id="menuitemsTEST"]');
+                        // Realiza a busca
+                        await page.waitForSelector('[href="ft5/collect.aspx"]', { timeout: 5000 });
                         await page.click('[href="ft5/collect.aspx"]');
+                        console.log('entrando na pesquisa');
 
-                        await this.delay(5000); // Espera 5 segundos
+                        await new Promise(resolve => setTimeout(resolve, 5000)); // Espera 5 segundos
 
                         await page.waitForSelector('[id="bt_search"]');
                         await page.click('[id="bt_search"]');
 
-                        
                         await this.delay(5000); // Espera 5 segundos
 
+                        // Executa a consulta SQL para obter os clientes
                         const result = await this.sql.query`SELECT * FROM baseRobo`;
-                        // Verifica se há resultados
-                        if (result && result.recordset && result.recordset.length > 0) {
-                            // Itera sobre cada linha retornada
-                            for (const [index, row] of result.recordset.entries()) {
-                                if (row.agencia == 'SFIN XXV' && row.desc_evento == '-') {
-                                    console.log(row);
-                                    setTimeout(async () => {
-                                        console.log(`Ação executada para a linha ${index + 1}`);
-                                        await page.type('[id="txt_CollectSearch"]', '13185479'); //36015470 erro de 2 contratos ou mais 
-                                        console.log('Pesquisar após 5 segundos');
-                                        await this.delay(5000); // Espera 5 segundos
-                                        contador++; // Incrementa o contador para ir para o próximo cliente na próxima iteração
-                                    }, (index + 1) * 5000);
-                                }
-                            }
-                        } else {
-                            console.log("Nenhum resultado encontrado.");
+                        const clientes = [];
+                        for (const [index, row] of result.recordset.entries()) {
+                            clientes.push(row);
                         }
+
+                        // Loop sobre os clientes
+                        for (let i = 0; i < clientes.length; i++) {
+                            if (clientes[i].agencia == usuario.cartera && clientes[i].desc_evento == 'ok') {
+
+                                this.cliente = clientes[i];
+                                console.log('carteira ok', this.cliente);
+
+                                await page.type('[id="txt_CollectSearch"]', this.cliente.operacao);
+                                await this.delay(5000); // Espera 5 segundos
+
+                                await page.keyboard.press('Enter');
+                                await this.delay(5000);
+
+                                // Encontra todos os elementos <a> na página
+                                const links = await page.$$('a');
+
+                                // Verifica se algum dos links tem o texto do cliente
+                                for (const link of links) {
+                                    const textoLink = await page.evaluate(element => element.textContent.trim(), link);
+                                    if (textoLink === this.cliente.cliente) {
+                                        // Tenta clicar no link com o texto do cliente
+                                        try {
+                                            await link.click();
+                                            console.log('Clicou no link com o texto', this.cliente.cliente);
+                                            await this.delay(5000); // Espera 5 segundos
+                                            break;
+                                        } catch (error) {
+                                            console.error('Erro ao clicar no link:');
+                                            break;
+                                        }
+                                    }else{
+                                        console.log('link diferente',textoLink);
+                                        console.log('nome diferente',this.cliente.cliente);
+                                    }
+                                }
+
+                                console.log('continuando!!');
+
+                                const conteudoTagsB = await page.$$eval('b', elements => elements.map(element => element.textContent.trim()));
+
+                                const textoEsperado = 'Contas em Assessorias Externas - NÃO DISPONÍVEIS';
+
+                                if (conteudoTagsB.includes(textoEsperado)) {
+                                    console.log(`O texto "${textoEsperado}" foi encontrado dentro das tags <b>.`);
+                                    await this.delay(5000); // Espera 5 segundos
+
+                                    // Imprime o ID do cliente atualizar para verificação
+                                    var clienteAtualizar = this.cliente;
+                                    console.log('ID do cliente a ser atualizado:', clienteAtualizar.operacao);
+
+                                    // Atualiza a obs_evento do cliente para fora da base no banco de dados
+                                    const atualizarClienteQuery = `UPDATE baseRobo SET desc_evento = 'fora da base' WHERE operacao = ${clienteAtualizar.operacao}`;
+                                    await this.sql.query(atualizarClienteQuery);
+
+                                    // Vai para o começo do while
+                                    break;
+                                } else {
+                                    console.log(`O texto "${textoEsperado}" não foi encontrado dentro das tags <b>.`);
+
+                                    this.selecionarMensagem();
+                                    //let mensagem = this.selecionarMensagem();
+                                    let mensagem = 'Telefone indisponível no momento';
+                                    console.log(mensagem);
+                                    await page.type('[id="TaskResults"]', `${mensagem}`);
+                                    if(mensagem == 'Telefone indisponível no momento'){
+                                        console.log('Pressionou Enter. Aguardando 5 segundos...');
+                                        await new Promise(resolve => setTimeout(resolve, 5000));
+                                        await page.keyboard.press('ArrowDown');
+                                        await page.keyboard.press('Enter');
+                                        console.log('Esperou 5 segundos.');
+                                        await page.click('[id="bt_SaveWorkflowTracking"]');
+                                        await new Promise(resolve => setTimeout(resolve, 5000));
+                                        
+                                        var clienteAtualizar = this.cliente;
+                                    console.log('ID do cliente a ser atualizado:', clienteAtualizar.operacao);
+
+                                    // Atualiza a obs_evento do cliente para fora da base no banco de dados
+                                    const atualizarClienteQuery = `UPDATE baseRobo SET desc_evento = 'fora da base' WHERE operacao = ${clienteAtualizar.operacao}`;
+                                    await this.sql.query(atualizarClienteQuery);
+                                    await new Promise(resolve => setTimeout(resolve, 180000));
+                                    }
+                                }
+                                break;
+                            } else {
+                                console.log("carteira acabou!!!");
+                                continue;
+                            }
+                        }
+                        console.log('saiu do for');
+                        await page.goto('https://alpheratz.itapevarec.com.br/Alpheratz/login.aspx');
                     }
                 } else {
                     console.log("fora do horario de serviço");
                     break; // Sai do loop se estiver fora do horário de serviço
                 }
             }
-    
-            await browser.close(); // Fecha o navegador após o loop
         } catch (erro) {
             console.error('Ocorreu um erro:', erro);
         }
     }
     
-    
-    
-
-    delay(ms) {
+    // Função de atraso
+    async delay(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    //Função para selecionar aleatoriamente uma mensagem com base nas porcentagens
+    // Função para selecionar aleatoriamente uma mensagem com base nas porcentagens
     selecionarMensagem() {
-// Array com as mensagens disponíveis
-const mensagensGrupo1 = [
-    'Telefone ocupado',
-    'Telefone não atende',
-    'Telefone indisponível no momento',
-    'Contato desligou',
-    'Desconhece o cliente',
-    'Ligação muda'
-];
+        // Array com as mensagens disponíveis
+        const mensagensGrupo1 = [
+            'Telefone ocupado',
+            'Telefone não atende',
+            'Telefone indisponível no momento',
+            'Contato desligou',
+            'Desconhece o cliente',
+            'Ligação muda'
+        ];
 
-const mensagensGrupo2 = [
-    'Ligação interrompida/ruim',
-    'Solicita contato outro dia/hora',
-    'Recado'
-];
+        const mensagensGrupo2 = [
+            'Ligação interrompida/ruim',
+            'Solicita contato outro dia/hora',
+            'Recado'
+        ];
 
-// Função para selecionar aleatoriamente uma mensagem com base nas porcentagens
-    const random = Math.random(); // Gerar um número aleatório entre 0 e 1
+        // Função para selecionar aleatoriamente uma mensagem com base nas porcentagens
+        const random = Math.random(); // Gerar um número aleatório entre 0 e 1
 
-    // Selecionar uma mensagem com base na porcentagem
-    if (random > 0.2) {
-        // 80% das vezes, seleciona uma mensagem do primeiro grupo
-        return mensagensGrupo1[Math.floor(Math.random() * mensagensGrupo1.length)];
-    } else if(random <= 0.2) {
-        // 20% das vezes, seleciona uma mensagem do segundo grupo
-        return mensagensGrupo2[Math.floor(Math.random() * mensagensGrupo2.length)];
+        // Selecionar uma mensagem com base na porcentagem
+        if (random > 0.2) {
+            // 80% das vezes, seleciona uma mensagem do primeiro grupo
+            return mensagensGrupo1[Math.floor(Math.random() * mensagensGrupo1.length)];
+        } else if(random <= 0.2) {
+            // 20% das vezes, seleciona uma mensagem do segundo grupo
+            return mensagensGrupo2[Math.floor(Math.random() * mensagensGrupo2.length)];
+        }
     }
-
-// Chame a função selecionarMensagem() para obter uma mensagem aleatória
-console.log('Mensagem selecionada:', mensagem);
-
-}
 
 }
 
--- main.js
+++ main.js
@@ -1,32 +1,15 @@
 const { app, BrowserWindow } = require('electron');
-const { spawn } = require('child_process');
 
 function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
+      nodeIntegration: true,
+      contextIsolation: false,
     }
   });
   mainWindow.loadFile('index.html');
-
-  //Iniciar o servidor Python como um processo secundário
-  const pythonProcess = spawn('python', ['./robo.py']);
-
-  // Exibir qualquer saída do processo Python no console
-  pythonProcess.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
-
-  pythonProcess.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-
-  pythonProcess.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
 }
 
 app.whenReady().then(createWindow);
